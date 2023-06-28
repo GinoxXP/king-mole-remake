703eabc8c7096909bed1701a2c700f7d7bf9ff6e
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-06-28 17:05:30 +0300

    Fix berserker cant move behaviour

--- Assets/Pushed/Enemy/Berserker/Scripts/Berserker.cs
+++ Assets/Pushed/Enemy/Berserker/Scripts/Berserker.cs
@@ -4,15 +4,30 @@ public class Berserker : AEnemy
 {
     private bool isWeak;
 
+    protected override void CantMove()
+    {
+        if (!CanInteract())
+            return;
+
+        base.CantMove();
+    }
     protected override void Move(Vector2 direction)
+    {
+        if (!CanInteract())
+            return;
+
+        base.Move(direction);
+        isWeak = false;
+    }
+
+    private bool CanInteract()
     {
         if (!isWeak)
         {
             isWeak = true;
-            return;
+            return false;
         }
-
-        base.Move(direction);
-        isWeak = false;
+        else
+            return true;
     }
 }
