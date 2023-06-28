f282a5e64234d4e3089924d0abd91a908eec2e6b
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-06-28 17:01:13 +0300

    Add wizard

---
+++ Assets/Pushed/Enemy/Wizard/Scripts/Wizard.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Wizard : AEnemy
+{
+    private static readonly int MAX_SHIELD_STATUS = 2;
+
+    private bool isCanUseShield = true;
+    private int shieldStatus;
+
+    protected override void CantMove()
+    {
+        if (!CanInteract())
+            return;
+
+        base.CantMove();
+    }
+
+    protected override void Move(Vector2 direction)
+    {
+        if (!CanInteract())
+            return;
+
+        base.Move(direction);
+    }
+
+    private bool CanInteract()
+    {
+        if (isCanUseShield)
+        {
+            isCanUseShield = false;
+            shieldStatus = MAX_SHIELD_STATUS;
+            return false;
+        }
+
+        if (shieldStatus != 0)
+            return false;
+
+        return true;
+    }
+}
