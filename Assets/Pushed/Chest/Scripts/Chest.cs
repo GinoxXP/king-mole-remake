e6de40e1f85b60d168df08a6c6c5ce893c20dcd2
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-06-28 15:54:39 +0300

    Hotfix chest

--- Assets/Pushed/Chest/Scripts/Chest.cs
+++ Assets/Pushed/Chest/Scripts/Chest.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class Chest : AMoved
 {
 
-    public override void CantMove()
+    protected override void CantMove()
     {
     }
 
-    public override void Move(Vector2 direction)
+    protected override void Move(Vector2 direction)
     {
         var targetPosition = transform.position + (Vector3)direction;
         transform
