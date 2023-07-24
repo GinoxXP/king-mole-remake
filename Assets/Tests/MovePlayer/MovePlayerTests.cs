da99f6c1044f51559ed62b2a116e28edc205470c
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-07-24 22:14:45 +0300

    Add test move player with wall detect

--- Assets/Tests/MovePlayer/MovePlayerTests.cs
+++ Assets/Tests/MovePlayer/MovePlayerTests.cs
@@ -7,37 +7,72 @@ using NUnit.Framework;
 public class MovePlayerTests : SceneTestFixture
 {
     [UnityTest]
-    public IEnumerator MovePlayer_PositionChage_FalseReturn()
+    public IEnumerator MovePlayer_PositionChange_FalseReturn()
     {
         yield return LoadScene("Test_MovePlayer_PositionChanged");
 
         yield return new WaitForSeconds(1.0f);
 
         var player = SceneContainer.Resolve<Player>();
-        var startPosition = player.transform.position;
 
         player.SetMoveDirection(Vector2.up);
-
         yield return new WaitForSeconds(1.0f);
 
         player.SetMoveDirection(Vector2.down);
-
         yield return new WaitForSeconds(1.0f);
 
         player.SetMoveDirection(Vector2.left);
-
         yield return new WaitForSeconds(1.0f);
 
         player.SetMoveDirection(Vector2.right);
-
         yield return new WaitForSeconds(1.0f);
 
         player.SetMoveDirection(Vector2.right);
-
         yield return new WaitForSeconds(1.0f);
 
         var endPosition = player.transform.position;
 
-        Assert.IsTrue(endPosition.y == startPosition.y && endPosition.x > startPosition.x);
+        Assert.IsTrue(endPosition == new Vector3(1, 0));
+    }
+
+    [UnityTest]
+    public IEnumerator MovePlayerWithWallDetect_PositionChange_FalseReturn()
+    {
+        yield return LoadScene("Test_MovePlayerWithWallDetect_PositionChange");
+
+        yield return new WaitForSeconds(1.0f);
+
+        var player = SceneContainer.Resolve<Player>();
+
+        player.SetMoveDirection(Vector2.up);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.left);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.down);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.down);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.down);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.left);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.right);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.down);
+        yield return new WaitForSeconds(1.0f);
+
+        player.SetMoveDirection(Vector2.right);
+        yield return new WaitForSeconds(1.0f);
+
+        var endPosition = player.transform.position;
+
+        Assert.IsTrue(endPosition == new Vector3(1, -2));
     }
 }
