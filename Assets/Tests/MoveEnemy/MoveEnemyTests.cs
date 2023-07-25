98aa117c1d6aa9feef80c58ad4f2571a6c586b51
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-07-25 22:17:41 +0300

    Add move peasant test

---
+++ Assets/Tests/MoveEnemy/MoveEnemyTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zenject;
+
+public class MoveEnemyTests : SceneTestFixture
+{
+    [UnityTest]
+    public IEnumerator MovePeasant_PositionChange_FalseReturn()
+    {
+        yield return LoadScene("Test_MovePeasant_PositionChanged");
+        yield return new WaitForSeconds(1.0f);
+
+        var player = SceneContainer.Resolve<Player>();
+
+        var peasant = GameObject.FindObjectOfType<Peasant>();
+
+        player.SetMoveDirection(Vector3.right);
+        yield return new WaitForSeconds(1.0f);
+
+        Assert.IsTrue(
+            player.transform.position == Vector3.zero &&
+            peasant.transform.position == new Vector3(2, 0));
+    }
+}
