ee5e17f2a69b6e0dc3c2a0d1205aacb9dbf1750b
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-08-30 22:00:42 +0300

    Add chest animation

--- Assets/Pushed/Chest/Scripts/Chest.cs
+++ Assets/Pushed/Chest/Scripts/Chest.cs
@@ -1,6 +1,19 @@
+using System;
+using UnityEngine;
+
+[RequireComponent (typeof(Animator))]
 public class Chest : AMoved
 {
-    protected override void CantMove()
+    private Animator animator;
+
+    public override void Push(Player player, Vector2 direction, Action action)
     {
+        base.Push(player, direction, action);
+        animator.Play("Push");
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
     }
 }
